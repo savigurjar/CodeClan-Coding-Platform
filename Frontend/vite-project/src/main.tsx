5154ed5d07f1d3d6b04cef05b03a8da1289d5fe1
savigurjar <savigurjar1201@gmail.com>
2025-11-10 22:12:25 +0530

    done

--- Frontend/vite-project/src/main.tsx
+++ Frontend/vite-project/src/main.tsx
@@ -1,16 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router"; 
+import { BrowserRouter } from "react-router";
 import { ThemeProvider } from "./components/theme-provider";
+import { Provider } from "react-redux"; 
+import store from "./store/store.ts";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
